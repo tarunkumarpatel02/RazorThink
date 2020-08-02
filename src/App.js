a4556e45f22a1f1c4545a46e6bd655fb958171d8
tarunkumarpatel02 <tarunkpatel02@gmail.com>
2020-08-03 00:29:24 +0530

    modifications and fixes

--- src/App.js
+++ src/App.js
@@ -12,16 +12,93 @@ class App extends Component {
     Images : [],
     ImagesForSiblings : [],
     PageNo : 1,
-    searchKey: ''
+    searchKey: '',
+    aboutProps:{},
+    ImagePagination:[],
+    siblingPagination:[],
+    searchResult:[],
   }
 
   componentDidMount(){
     var store = []
-    axios.get("https://api.unsplash.com/photos/?client_id=QfXd71TsBLAcVluOiBe6ZJsltwJnTIV8WVjD24lovvE&query=water").then(response => {
+    axios.get("https://api.unsplash.com/photos/?client_id=QfXd71TsBLAcVluOiBe6ZJsltwJnTIV8WVjD24lovvE&query=water&per_page=9").then(response => {
         console.log(response.data);
-        response.data.splice(0,1);
+        // response.data.splice(0,1);
         this.setState({Images:response.data});
+        console.log(this.state.Images);
+        var ImagePagination  = this.state.Images.map(elem =>(
+          <Link to={{
+              pathname:elem.urls.full,
+              key:elem.id,
+              hash:elem.user.profile_image.small,
+              aboutProps:{
+                  imageUrl :elem.urls.full,
+                  profileImage:elem.user.profile_image.small,
+                  userName: elem.user.username,
+                  fullName:elem.user.first_name+' '+elem.user.last_name
+              }
+          }}>
+              <div className="grid-item">
+                  <img className="hvr" src={elem.urls.full}></img>
+                  <img className="profileImage" src={elem.user.profile_image.small}/> <p className="textOverImage">Image by <span className="user">{elem.user.first_name+' '+elem.user.last_name}</span></p>
+              </div>
+          </Link>
+      ));
+      this.setState({ImagePagination:ImagePagination});
     })    
+  }
+
+  // shouldComponentUpdate(nextProps, nextState){
+  //   return this.state.siblingPagination != nextState.siblingPagination;
+  // }
+  componentDidUpdate(){
+    console.log("component did update ran")
+      if(this.state.ImagesForSiblings.length !==0){
+      var  siblingPagination = this.state.ImagesForSiblings.map(elem =>(
+            <Link to={{
+                pathname:elem.urls.full,
+                key:elem.id,
+                hash:elem.user.profile_image.small,
+                aboutProps:{
+                    imageUrl :elem.urls.full,
+                    profileImage:elem.user.profile_image.small,
+                    userName: elem.user.username,
+                    fullName:elem.user.first_name+' '+elem.user.last_name
+                }
+            }}>
+                <div className="grid-item">
+                    <img className="hvr" src={elem.urls.full}></img>
+                    <img className="profileImage" src={elem.user.profile_image.small}/> <p className="textOverImage">Image by <span className="user">{elem.user.first_name+' '+elem.user.last_name}</span></p>
+                </div>
+            </Link>
+        ));
+
+        // ImagePagination = document.querySelector(".grid-container");
+      //  this.setState({ImagePagination:[...this.state.ImagePagination,siblingPagination]});
+       this.setState({ImagePagination:this.state.ImagePagination.concat(siblingPagination)});
+       this.state.ImagesForSiblings.length = 0;
+      }else if(this.state.searchResult.length !==0){
+        var ImagePagination  = this.state.searchResult.map(elem =>(
+          <Link to={{
+              pathname:elem.urls.full,
+              key:elem.id,
+              hash:elem.user.profile_image.small,
+              aboutProps:{
+                  imageUrl :elem.urls.full,
+                  profileImage:elem.user.profile_image.small,
+                  userName: elem.user.username,
+                  fullName:elem.user.first_name+' '+elem.user.last_name
+              }
+          }}>
+              <div className="grid-item">
+                  <img className="hvr" src={elem.urls.full}></img>
+                  <img className="profileImage" src={elem.user.profile_image.small}/> <p className="textOverImage">Image by <span className="user">{elem.user.first_name+' '+elem.user.last_name}</span></p>
+              </div>
+          </Link>
+      ));
+      this.setState({ImagePagination:ImagePagination});
+      this.state.searchResult.length = 0;
+      }
   }
 
   onchangeHandler=(e)=>{
@@ -29,10 +106,9 @@ class App extends Component {
   }
 
   searchHandler=(e)=>{
-    axios.get("https://api.unsplash.com/photos/?client_id=QfXd71TsBLAcVluOiBe6ZJsltwJnTIV8WVjD24lovvE&query="+this.state.searchKey).then(response => {
+    axios.get("https://api.unsplash.com/photos/?client_id=QfXd71TsBLAcVluOiBe6ZJsltwJnTIV8WVjD24lovvE&per_page=9&query="+this.state.searchKey).then(response => {
         console.log(response.data);
-        response.data.splice(0,1);
-        this.setState({Images:response.data});
+        this.setState({searchResult:response.data});
     })   
   }
 
@@ -54,22 +130,9 @@ class App extends Component {
         PageNo: state.PageNo+1
        };
      });
-     axios.get("https://api.unsplash.com/photos/?client_id=QfXd71TsBLAcVluOiBe6ZJsltwJnTIV8WVjD24lovvE&page="+currentPage).then(response => {
+     axios.get("https://api.unsplash.com/photos/?client_id=QfXd71TsBLAcVluOiBe6ZJsltwJnTIV8WVjD24lovvE&per_page=9&page="+currentPage).then(response => {
         console.log(response.data);
-        response.data.splice(0,1);
         this.setState({ImagesForSiblings:response.data});
-        var Pagination = response.data.map(elem =>{
-              var Image = document.createElement("img");
-              var Link = document.createElement("a")
-              Link.setAttribute("href","/"+elem.urls.full);
-              Image.setAttribute("class","hvr");
-              var DIV   = document.createElement("div");
-              DIV.setAttribute("class","grid-item");
-             Image.setAttribute("src",elem.urls.full);
-             DIV.appendChild(Image);
-             Link.appendChild(DIV);
-             currentEvent.appendChild(Link);
-        });
     });        
 }
 
@@ -77,7 +140,7 @@ class App extends Component {
     return (
       <div className="App">
          <Switch>
-             <Route path={'/'} exact render = {() => <LandingPage Images={this.state.Images} onchangeHandler={this.onchangeHandler} searchHandler={this.searchHandler} ImagesForSiblings={this.state.ImagesForSiblings} LoadMore={this.LoadMore}/>}/>
+             <Route path={'/'} exact render = {() => <LandingPage ImagePagination={this.state.ImagePagination} onchangeHandler={this.onchangeHandler} searchHandler={this.searchHandler} ImagesForSiblings={this.state.ImagesForSiblings} LoadMore={this.LoadMore}/>}/>
              <Route path={'/:title'} render = {(props) => <ImageDownload  Imagedownloaded={this.ImageDownload} {...props}/>}/>
          </Switch>
       </div>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,61 +1,71 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import  {FaSearch} from 'react-icons/fa';
 
 import './LandingPage.css';
 
 
-class LandingPage extends Component{
-  render(){
-      var ImagePagination = this.props.Images.map(elem =>(
-                <Link to={{
-                    pathname:elem.urls.full,
-                    key:elem.id,
-                    hash:elem.user.profile_image.small,
-                    aboutProps:{
-                        imageUrl :elem.urls.full,
-                        profileImage:elem.user.profile_image.small,
-                        userName: elem.user.username,
-                        fullName:elem.user.first_name+' '+elem.user.last_name
-                    }
-                }}>
-                    <div className="grid-item">
-                        <img className="hvr" src={elem.urls.full}></img>
-                        <img className="profileImage" src={elem.user.profile_image.small}/> <p className="textOverImage">Image by <span className="user">{elem.user.first_name+' '+elem.user.last_name}</span></p>
-                    </div>
-                </Link>
+const LandingPage=(props)=>{
 
-      ));
-
-    //   if(this.props.ImagesForSiblings.length !==0){
-    //     var siblingPagination = this.props.ImagesForSiblings.map(elem =>(
+    // var ImagePagination = document.getElementsByClassName("grid-container");
+     
+    //     if(props.ImagesForSiblings.length ===0){
+    //         var ImagePagination  = props.Images.map(elem =>(
+    //             <Link to={{
+    //                 pathname:elem.urls.full,
+    //                 key:elem.id,
+    //                 hash:elem.user.profile_image.small,
+    //                 aboutProps:{
+    //                     imageUrl :elem.urls.full,
+    //                     profileImage:elem.user.profile_image.small,
+    //                     userName: elem.user.username,
+    //                     fullName:elem.user.first_name+' '+elem.user.last_name
+    //                 }
+    //             }}>
+    //                 <div className="grid-item">
+    //                     <img className="hvr" src={elem.urls.full}></img>
+    //                     <img className="profileImage" src={elem.user.profile_image.small}/> <p className="textOverImage">Image by <span className="user">{elem.user.first_name+' '+elem.user.last_name}</span></p>
+    //                 </div>
+    //             </Link>
+    //         ));
+    //     }        
+    //   if(props.ImagesForSiblings.length !==0){
+    //   var  siblingPagination = props.ImagesForSiblings.map(elem =>(
     //         <Link to={{
     //             pathname:elem.urls.full,
-    //             key:elem.id
+    //             key:elem.id,
+    //             hash:elem.user.profile_image.small,
+    //             aboutProps:{
+    //                 imageUrl :elem.urls.full,
+    //                 profileImage:elem.user.profile_image.small,
+    //                 userName: elem.user.username,
+    //                 fullName:elem.user.first_name+' '+elem.user.last_name
+    //             }
     //         }}>
     //             <div className="grid-item">
     //                 <img className="hvr" src={elem.urls.full}></img>
+    //                 <img className="profileImage" src={elem.user.profile_image.small}/> <p className="textOverImage">Image by <span className="user">{elem.user.first_name+' '+elem.user.last_name}</span></p>
     //             </div>
     //         </Link>
     //     ));
 
-    //     ImagePagination = ImagePagination.appendChild(siblingPagination)
+    //     // ImagePagination = document.querySelector(".grid-container");
+    //    ImagePagination = ImagePagination.concat(siblingPagination);
     //   }
 
         return(
             <div>
                 <div className="searchImg">
-                    <input type="text" className="textBox" name="search" onChange={this.props.onchangeHandler} placeholder="Search for images here..."/><FaSearch onClick={this.props.searchHandler} className="searchIcon"/>
+                    <input type="text" className="textBox" name="search" onChange={props.onchangeHandler} placeholder="Search for images here..."/><FaSearch onClick={props.searchHandler} className="searchIcon"/>
                  </div>
                 <div className="grid-container">
-                    {ImagePagination}
+                    {props.ImagePagination}
                 </div>
                 <div className="Loadmore">
-                    <button className="loadmoreBtn" onClick={this.props.LoadMore}>Load More</button>
+                    <button className="loadmoreBtn" onClick={props.LoadMore}>Load More</button>
                 </div>
             </div>
         );
     }
-}
 
 export default LandingPage;
